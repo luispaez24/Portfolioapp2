6dabf0803c04fdb167a3307089fee0c75af5d209
luispaez24 <74021581+luispaez24@users.noreply.github.com>
2024-09-24 16:19:37 -0600

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ApplicationRecord
+
+ has_one_attached :avatar
+
+
+  validates :school_email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
+
+
+  
 end
